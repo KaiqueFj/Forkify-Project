d3eac37b1f3c9f7fc0137d7e1af352a7220478d6
KaiqueFj <kaiquelferraz@hotmail.com>
2023-02-18 04:56:41 -0300

    Feat: Finished Project

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = `3b55c939-6edd-406a-8c54-97784e8abed5`;
+export const MODAL_CLOSE_SEC = 2.5;
